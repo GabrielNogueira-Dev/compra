e0b645688d89558e0812e779e6c79f5ada00e36d
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2025-05-28 22:13:46 +0100

    criacao de rotas

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+
+
+import { Outlet } from "react-router-dom";
+import { Header } from "../header";
+
+
+export function Layout() {
+ 
+  return (
+      <>
+      <Header/>
+      <Outlet/>
+      </>
+    
+  )
+}
+
---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,8 @@
+
+export function Cart(){
+    return(
+      <div>
+        <h1>Hello World</h1>
+      </div>  
+    )
+}

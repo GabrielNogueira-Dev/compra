000c3d875cffaed6f8ef6ce10b5c01834f031ef1
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2025-05-29 21:41:28 +0100

    Axios e Api renderizando na Home e dados em funcionamento direto

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,33 +1,66 @@
-
+import { useEffect,useState } from "react"
 import { BsCartPlus } from "react-icons/bs"
 
+import { api } from "../../services/api"
+
+interface ProductsProps{
+    id:number;
+    title:string;
+    description:string;
+    price:number;
+    cover:string
+}
+
 export function Home(){
+const [products,setProducts] = useState<ProductsProps[]>([])
+
+useEffect(()=>{
+
+async function getProducts() {
+ const response = await api.get("/products")   
+setProducts(response.data)
+
+
+}
+
+getProducts();
+},[])
+
+
     return(
+    
       <div>
         <main className="w-full max-w-7xl px-4 mx-auto">
         <h1 className="rounded bg-gray-300 font-bold text-2xl mb-4 mt-10 text-center">
            Produtos em Alta </h1>
        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-        <section className="w-full">
-<img className="w-full rounded-lg max-h-72 mb-2"
-src="https://www.bing.com/images/search?q=imagem+fone&id=03BC7778D6DEBFDF0E63C74CF37347293C16991B&FORM=IACFIR" alt="logo" />
-<p className="font-medium mt-1 mb-2">Airpods Pro</p>
+       {products.map( (product) => (
+            <section key={product.id}
+             className="w-full">
 
-<div className="flex gap-3 items-center">
-    <strong className="text-zinc-700/90">
-        R$ 1000
-    </strong>
-    <button className="bg-zinc-900 p-1 rounded">
-<BsCartPlus/>
-    </button>
+    <img className="w-full rounded-lg max-h-72 mb-2"
+    src={product.cover} alt={product.title} />
+    <p className="font-medium mt-1 mb-2"> {product.title} </p>
 
-</div>
+    <div className="flex gap-3 items-center">
+        <strong className="text-zinc-700/90">
+           {product.price.toLocaleString("pt-BR",{
+            style:"currency",
+            currency:"BRL"
+           })}
+        </strong>
+        <button className="bg-zinc-900 p-1 rounded">
+    <BsCartPlus/>
+        </button>
 
-        </section>
-       </div>
+    </div>
 
-        
-        </main>
-      </div>  
-    )
-}
+            </section>
+        ))}
+        </div>
+
+            
+            </main>
+        </div>  
+        )
+    }
